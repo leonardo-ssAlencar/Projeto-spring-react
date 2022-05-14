06abb8db7bdeac063e91e9c9229a036e69a4b8cd
leonardo-ssAlencar <leonardo.souza.santosalencar@gmail.com>
2022-05-13 21:25:10 -0300

    props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,53 +6,44 @@ import { MoviePage } from "types/Movies";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
-  
+
 
   const [pageNumber, setPageNumber] = useState(0);
-   
-  useEffect( () => {axios.get(`${BASE_URL}/movies?page=0&size=12`).then(r => 
-    {
-      const data = r.data as MoviePage;
-      setPageNumber(data.number+1)
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true
+  });
 
-    })}, []);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then(r => {
+      const data = r.data
+      setPage(data);
+
+    })
+  }, [pageNumber]);
 
   return (
     <>
-      <p>{pageNumber}</p>
       <Pagination />
 
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          
-          
+          {page.content.map(item =>
+          (<div key={item.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard movie={item} />
+          </div>))}
         </div>
       </div>
 
-      
+
 
     </>
   );
